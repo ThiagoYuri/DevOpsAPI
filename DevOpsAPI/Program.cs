9df600dde13df00f6e0c757fc253347c51d00d1b
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-12-23 11:51:51 -0300

    Add: logic in program main

--- DevOpsAPI/Program.cs
+++ DevOpsAPI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DevOpsAPI;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+
+QueryExecutor query = new QueryExecutor("organization","token");
+
+//Task<IList<WorkItem>> workItens = query.QueryOpenBugs("Vitrine de Projetos");
+await query.PrintOpenBugsAsync("Name Project");
+
+Console.ReadKey();
